50e76fd30fc8813aabe4b947ff4bca820b4e4e1d
woaf <bfazekas@inf.elte.hu>
2019-04-22 13:05:39 +0200

    Switch between fullscreen and windowed mode. GL init moved to its own function.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,92 @@ static const unsigned int WIDTH = 1280;
 static const unsigned int HEIGHT = 720;
 
 static bool fullscreen = false;
+static bool hasToSwitch = false;
 
 void handleKeyboard(GLFWwindow* window, int key, int code, int action, int mode)
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
-        glfwSetWindowShouldClose(window, GL_TRUE);
+        if(fullscreen)
+        {
+            fullscreen = !fullscreen;
+            hasToSwitch = true;
+        }
+        else
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
+    if(key == GLFW_KEY_F && action == GLFW_PRESS && mode == GLFW_MOD_SHIFT)
+    {
+        fullscreen = !fullscreen;
+        hasToSwitch = true;
     }
 }
+
+void checkForFullscreen(GLFWwindow*& window, bool& windowInit)
+{
+    if(hasToSwitch || windowInit)
+    {
+        hasToSwitch = false;
+        glfwDestroyWindow(window);
+        if(fullscreen)
+        {
+            GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+            const GLFWvidmode* vidmode = glfwGetVideoMode(monitor);
+
+            if(vidmode != nullptr)
+            {
+                window = glfwCreateWindow(vidmode->width, vidmode->height, TITLE.c_str(), monitor, nullptr);
+            }
+        }
+        else
+        {
+            window = glfwCreateWindow(WIDTH, HEIGHT, TITLE.c_str(), nullptr, nullptr);
+        }
+        glfwMakeContextCurrent(window);
+        glfwSetKeyCallback(window, handleKeyboard);
+    }
+
+    if(windowInit)
+        windowInit = false;
+}
+
+bool initOpenGL(GLFWwindow*& window, bool& windowInit)
+{
+    if(!glfwInit())
+    {
+        return false;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    checkForFullscreen(window, windowInit);
+
+    if(window == nullptr)
+    {
+        glfwTerminate();
+        return false;
+    }
+
+    glewExperimental = GL_TRUE;
+
+    if(glewInit() != GLEW_OK)
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return false;
+    }
+    glClearColor(0.23f, 0.23f, 0.65f, 1.0);
+
+    return true;
+}
+
+
 
 void showFPS(GLFWwindow* window)
 {
@@ -37,10 +115,10 @@ void showFPS(GLFWwindow* window)
         outs.precision(3);
         outs << std::fixed
              << TITLE
-             << "  "
+             << " - "
              << "FPS: "
              << fps
-             << "  "
+             << " - "
              << "Frame time: "
              << msPerFrame
              << "(ms)";
@@ -53,53 +131,18 @@ void showFPS(GLFWwindow* window)
 
 int main()
 {
-    if(!glfwInit())
-        return 1;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
     GLFWwindow* window = nullptr;
-    if(fullscreen)
-    {
-        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-        const GLFWvidmode* vidmode = glfwGetVideoMode(monitor);
-
-        if(vidmode != nullptr)
-        {
-            window = glfwCreateWindow(vidmode->width, vidmode->height, TITLE.c_str(), monitor, nullptr);
-        }
-    }
-    else
-    {
-        window = glfwCreateWindow(WIDTH, HEIGHT, TITLE.c_str(), nullptr, nullptr);
-    }
-
-    if(window == nullptr)
-    {
-        glfwTerminate();
-        return 2;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, handleKeyboard);
-    glewExperimental = GL_TRUE;
-
-    if(glewInit() != GLEW_OK)
-    {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 3;
-    }
+    bool windowInit = true;
+    if(!initOpenGL(window, windowInit))
+        return -1;
 
     while(!glfwWindowShouldClose(window))
     {
         showFPS(window);
         glfwPollEvents();
 
-        glClearColor(0.23f, 0.23f, 0.65f, 1.0);
+        checkForFullscreen(window, windowInit);
+
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
